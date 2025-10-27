c185080ce10c2eaab46f6f612539fa745c806541
Vyankatesh Pagare <venkypagare9@gmail.com>
2025-10-27 14:36:45 +0530

    Update Category and Introduced RequestMapping

--- src/main/java/com/ecommerce/sbecom/controller/CategoryController.java
+++ src/main/java/com/ecommerce/sbecom/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.ecommerce.sbecom.controller;
 
 import com.ecommerce.sbecom.model.Category;
 import com.ecommerce.sbecom.service.CategoryService;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class CategoryController {
     // @Autowired
     private final CategoryService categoryService;
@@ -17,19 +19,22 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping("/api/public/categories")
+    // @GetMapping("/public/categories")
+    @RequestMapping(value = "/public/categories", method = RequestMethod.GET)
     public ResponseEntity<List<Category>> getAllCategories() {
         List<Category> categories = categoryService.getAllCategories();
         return new  ResponseEntity<>(categories, HttpStatus.OK);
     }
 
-    @PostMapping("/api/admin/category")
+    @PostMapping("/public/category")
+    // @RequestMapping(value = "/public/category", method = RequestMethod.POST)
     public ResponseEntity<String> createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category added successfully", HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/api/admin/categories/{categoryId}")
+    @DeleteMapping("/admin/categories/{categoryId}")
+    // @RequestMapping(value = "/admin/categories/{categoryId}", method = RequestMethod.DELETE)
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
          try {
              String status = categoryService.deleteCategory(categoryId);
@@ -39,5 +44,12 @@ public class CategoryController {
          } catch (ResponseStatusException e) {
              return new ResponseEntity<>(e.getReason(), e.getStatusCode());
          }
+    }
+
+    // @PutMapping("/public/category/{categoryId}")
+    @RequestMapping(value = "/public/category/{categoryId}", method = RequestMethod.PUT)
+    public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long categoryId) {
+        categoryService.updateCategory(category, categoryId);
+        return new ResponseEntity<>("Category updated successfully", HttpStatus.OK);
     }
 }
--- src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -30,5 +31,18 @@ public class CategoryServiceImpl implements CategoryService {
                 .findFirst().orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found"));
         categories.remove(category);
         return "Category with categoryId: " + categoryId + " deleted successfully !!";
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Optional<Category> optionalCategory = categories.stream().filter(c->c.getCategoryId()
+                        .equals(categoryId)).findFirst();
+        if (optionalCategory.isPresent()) {
+            Category existingCategory = optionalCategory.get();
+            existingCategory.setCategoryName(category.getCategoryName());
+            return existingCategory;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category Not Found");
+        }
     }
 }
