803d0dd43375a81354e0e01c7afcd4d5b2db0e58
Vyankatesh Pagare <venkypagare9@gmail.com>
2025-10-28 11:23:33 +0530

    Configuring our project for JPA

--- src/main/java/com/ecommerce/sbecom/controller/CategoryController.java
+++ src/main/java/com/ecommerce/sbecom/controller/CategoryController.java
@@ -2,7 +2,6 @@ package com.ecommerce.sbecom.controller;
 
 import com.ecommerce.sbecom.model.Category;
 import com.ecommerce.sbecom.service.CategoryService;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
